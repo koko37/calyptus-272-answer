3d35ab2a584468f0e3b641b6bd1fe9e6cf9ee237
koko37 <engel0088@gmail.com>
2024-01-20 17:58:55 +0800

    add script to search signer

---
+++ contracts/Tester.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+
+contract SignerTest {
+    function sign(address signer) public returns (bytes32) {
+        
+    }
+}
---
+++ scripts/search.js
@@ -0,0 +1,40 @@
+const { ethers } = require("ethers");
+const abi = new ethers.AbiCoder();
+
+async function main() {
+  console.log("start to search new address!");
+
+  let nonce = 203;
+  for (;;) {
+    const signer = ethers.keccak256(
+      abi.encode(["string", "uint256"], ["find", nonce])
+    );
+    const account = new ethers.Wallet(signer);
+
+    if (account.address.startsWith("0xface")) {
+      console.log(
+        "nonce: ",
+        nonce,
+        " \taddress: ",
+        account.address,
+        " \tsigner: ",
+        signer
+      );
+      break;
+    }
+
+    if (nonce % 100 == 0) {
+      console.log("till: ", nonce);
+    }
+    nonce++;
+  }
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  })
+  .then(() => {
+    console.log("Finished!");
+  });
